335de76b474754ae3ac0739ad25570e0dd96e251
Luke Sathrum <sathrumlu@butte.edu>
2018-03-08 11:33:42 -0800

    Adding Assignment 3b Files

---
+++ assignments/3b/distance.cpp
@@ -0,0 +1,11 @@
+/*
+ * Name        : distance.cpp
+ * Author      : NAME HERE
+ * Description : Implementation File for Distace Class
+ */
+// Include our .h file
+#include "distance.h"
+
+// Member Function Definitions
+
+// CODE HERE

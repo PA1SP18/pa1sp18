f9edde227bbd5a1295bf73b624d912e5ce047aad
Luke Sathrum <sathrumlu@butte.edu>
2018-03-30 12:32:42 -0700

    Adding Assignment 4 Files

---
+++ assignments/4/double_list.cpp
@@ -0,0 +1,6 @@
+#include "double_list.h"
+
+// The second part of creating a constant for our class
+const int DoubleList::kListCapacity;
+
+// YOUR MEMBER FUNCTION DEFINITIONS GO HERE
---
+++ assignments/4/double_list.h
@@ -0,0 +1,21 @@
+// OPENING HEADER COMMENTS GO HERE
+// HEADER GUARDS GO HERE
+// NEEDED INCLUDES GO HERE
+
+// CLASS DESCRIPTION GOES HERE
+class DoubleList {
+ public:
+   // PUBLIC MEMBER FUNCTIONS GO HERE 
+ private:
+  // This creates a constant to use for the size of the array, i.e. the maximum
+  // capacity of the list
+  static const int kListCapacity = 10;
+  // Create an Array of size kListCapacity.
+  // CODE TO CREATE THE ARRAY GOES HERE
+  
+  // Create a variable to hold the current size of the array. Note this means
+  // how many elements are being used in the array, not how many values it can
+  // hold
+  // CODE TO CREATE THE CURRENT SIZE VARIABLE
+};
+
